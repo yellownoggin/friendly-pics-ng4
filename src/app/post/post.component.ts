b1e6d5118b3f0dcdbf901759ee40f30ef0e8926d
John Hennelly <jjhennelly@gmail.com>
2017-10-16 10:33:51 -0400

    New: af5 displayDeletePost Ui

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,5 @@
 // TODO: why don't need import the observable operators in component
+import { AuthService } from '../shared/providers/auth.service';
 import { FriendlyFireService } from '../shared/friendly-fire.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
@@ -9,37 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
-  post: Object;
+  canDeletePost: boolean;
+  post: any;
   postId: string;
-/**
- * requirements: 
- * 0. need a post route with dynamic route  (x)
- * 1. url post/postId - this is set up in the (x)
- * 2. getPost for the post page
- * 3. -- need to get the post Id to use in component 
- * 
- */
- 
- 
 
-  constructor(private friendly: FriendlyFireService, private route: ActivatedRoute) {
+  constructor(private friendly: FriendlyFireService, private route: ActivatedRoute, private auth: AuthService) {
 
     // TODO: what is the pattern for getting service methods 
+    // the this quesiton
+    // delete if logic 
+    // if currentLogged in user is the user page id 
+    this.getCurrentPostData().switchMap((data) => {
+      console.log(data);
+      // bind the postData to post
+      this.post = data;
+     return this.displayDeleteButton(this.post.author);
+    }).subscribe((data) => {
+        // bind the displayDeleteButton value
+        console.log('can delete', data);
+        
+        this.canDeletePost = data;
+    });
+
     
    }
 
 
   ngOnInit() {
-      this.getCurrentPostData().subscribe((data) => {
-        console.log(data);
-       this.post = data;
-    },
-    (e) => {
-      console.log('error in currentpostdata', e);
-    },
-    () => {
-      console.log('complete');
-    });
+
+    // TODO: console error like multiple calls or something need to find solution
+    // took out the error and complete - wasn't working but also noisy what is the patter
+      // this.getCurrentPostData().switchMap((data) => {
+      //   console.log(data);
+      //   // bind the postData to post
+      //   this.post = data;
+      //  return this.displayDeleteButton(this.post.author);
+      // }).subscribe((data) => {
+      //     // bind the displayDeleteButton value
+      //     console.log('can delete', data);
+          
+      //     this.canDeletePost = data;
+      // });
+  
+
+    
   }
 
 
@@ -52,6 +66,17 @@ export class PostComponent implements OnInit {
       // return data;
       const $key = data.payload.key;
       return {$key, ...data.payload.val()};
+    });
+  }
+
+  // Displays delete if userlogged in & user is author of post
+  displayDeleteButton(author: any) {
+   return  this.auth.getAuthState().map((auth) => {
+     if (auth && author.uid === auth.uid) {
+       return true;
+     } else {
+       return false;
+     }
     });
   }
 
