e0c11c75fe52546914d123db798774479cc56bc2
John Hennelly <jjhennelly@gmail.com>
2017-11-03 15:24:58 -0400

    Re-factor: GoogleUserDataInterface filtered as in result.user 1 receiving information from google z user credential

--- src/app/model/more.ts
+++ src/app/model/more.ts
@@ -1,15 +1,23 @@
+// Used when google authorization data is filtered with result.user
+export interface GoogleUserData {
+	displayName: string;
+	photoURL: string;
+	uid: string;
+}
+
+
 // https://stackoverflow.com/questions/35789498/new-typescript-1-8-4-build-error-build-property-result-does-not-exist-on-t
 export interface FileReaderEventTarget extends EventTarget {
-    result: string;
+	result: string;
 }
 
 export interface FileReaderEvent extends Event {
-    target: FileReaderEventTarget;
-    getmessage(): string;
+	target: FileReaderEventTarget;
+	getmessage(): string;
 }
 
 export interface HTMLInputEvent extends Event {
-    target: HTMLInputElement & EventTarget;
+	target: HTMLInputElement & EventTarget;
 }
 // TODO: this is not right probably well definitely with any
 // below did not work so trundling it for now
@@ -19,23 +27,23 @@ export interface HTMLInputEvent extends Event {
 // }
 
 export interface PromiseWithDeferred extends PromiseConstructor {
-    deferred: object;
+	deferred: object;
 }
 
 
 export class PromiseD<T> extends Promise<T>  {
-    constructor(f: any) {
-        super(f);
-        Object.setPrototypeOf(this, PromiseD.prototype);
+	constructor(f: any) {
+		super(f);
+		Object.setPrototypeOf(this, PromiseD.prototype);
 
-    }
+	}
 
-    public deferred() {
-        const result: any = {};
-        result.promise = new Promise(function (resolve, reject) {
-            result.resolve = resolve;
-            result.reject = reject;
-        });
-        return result;
-    }
+	public deferred() {
+		const result: any = {};
+		result.promise = new Promise(function(resolve, reject) {
+			result.resolve = resolve;
+			result.reject = reject;
+		});
+		return result;
+	}
 }
