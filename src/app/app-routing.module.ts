5b143b47e698ba077491420a555d36b750508f98
John Hennelly <jjhennelly@gmail.com>
2017-11-07 23:20:59 -0500

    New: adding authguards to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,12 +10,17 @@ import { PostComponent } from './post/post.component';
 import { HomeFeedComponent } from './home-feed/home-feed.component';
 // import { SandComponent } from './sand/sand.component';
 
+import { AuthGuardService } from "./auth/auth-guard.service";
+import { UnauthGuardService } from "./auth/unauth-guard.service";
+import { WaitForAuthService } from "./auth/wait-for-auth.service";
+
+
 const routes: Routes = [
   { path: '', component: GeneralComponent },
   { path: 'addPicture', component: AddPictureComponent },
-  { path: 'user/:id', component: UserPageComponent },
+  { path: 'user/:id', component: UserPageComponent, canActivate: [WaitForAuthService] },
   { path: 'post/:postId', component: PostComponent },
-  { path: 'home', component: HomeFeedComponent }
+  { path: 'home', component: HomeFeedComponent, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({
