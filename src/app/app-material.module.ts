bd3d9c828e2a9dea12fcf06bd7304f26eaa501b0
John Hennelly <jjhennelly@gmail.com>
2017-11-10 23:24:01 -0500

    New: MaterialIO feed design 1

--- src/app/app-material.module.ts
+++ src/app/app-material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule,  MatAutocompleteModule } from '@angular/material';
+import { MatButtonModule, MatIconModule,  MatAutocompleteModule, MatCardModule } from '@angular/material';
 
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatAutocompleteModule],
-    exports: [MatButtonModule, MatIconModule, MatAutocompleteModule],
+    imports: [MatButtonModule, MatIconModule, MatAutocompleteModule, MatCardModule],
+    exports: [MatButtonModule, MatIconModule, MatAutocompleteModule, MatCardModule],
 })
 
 export class AppMaterialModule {}
